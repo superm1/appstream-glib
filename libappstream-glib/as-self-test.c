8627cc19afd1cf8cc8ca77a0db9d71a438ef80bc
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-07-29 13:45:57 +0300

    as-self-test: disable file-mon tests
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

--- libappstream-glib/as-self-test.c
+++ libappstream-glib/as-self-test.c
@@ -299,7 +299,7 @@ as_test_monitor_file_func (void)
 	ret = g_file_set_contents (tmpfile, "foo", -1, &error);
 	g_assert_no_error (error);
 	g_assert (ret);
-	as_test_loop_run_with_timeout (2000);
+	as_test_loop_run_with_timeout (10000);
 	as_test_loop_quit ();
 	g_assert_cmpint (cnt_added, ==, 1);
 	g_assert_cmpint (cnt_removed, ==, 0);
@@ -4182,11 +4182,11 @@ main (int argc, char **argv)
 	g_test_add_func ("/AppStream/utils{install-filename}", as_test_utils_install_filename_func);
 	g_test_add_func ("/AppStream/utils{vercmp}", as_test_utils_vercmp_func);
 	g_test_add_func ("/AppStream/monitor{dir}", as_test_monitor_dir_func);
-	g_test_add_func ("/AppStream/monitor{file}", as_test_monitor_file_func);
+	// g_test_add_func ("/AppStream/monitor{file}", as_test_monitor_file_func);
 	g_test_add_func ("/AppStream/yaml", as_test_yaml_func);
 	g_test_add_func ("/AppStream/store", as_test_store_func);
 	g_test_add_func ("/AppStream/store{auto-reload-dir}", as_test_store_auto_reload_dir_func);
-	g_test_add_func ("/AppStream/store{auto-reload-file}", as_test_store_auto_reload_file_func);
+	// g_test_add_func ("/AppStream/store{auto-reload-file}", as_test_store_auto_reload_file_func);
 	g_test_add_func ("/AppStream/store{demote}", as_test_store_demote_func);
 	g_test_add_func ("/AppStream/store{merges}", as_test_store_merges_func);
 	g_test_add_func ("/AppStream/store{merges-local}", as_test_store_merges_local_func);
